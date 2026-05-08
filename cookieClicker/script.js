3d959921c7631793c0a5d5299576bd98cd99dcb4
U-12031 <moro.yoshi0902@gmail.com>
2026-05-09 00:49:50 +0900

    cookieのポップアップまで作成

--- cookieClicker/script.js
+++ cookieClicker/script.js
@@ -1,1 +1,24 @@
+/** この関数は、idから要素を取得します
+ * @param {string} id 要素のID
+ * @return {HTMLElement} 取得された要素
+ */
 function el(id) {document.getElementById(id)}
+
+/** この関数は、keyとvalueを指定して、cookieを保存します
+ * @param {string} key cookieの名前
+ * @param {string} value cookieの値
+ * @returns {string} 保存されたcookie
+ */
+function save(key, value) {
+	document.cookie = `${key}=${value}; max-age=${60*60*24*365};`;
+	return document.cookie;
+}
+
+/** この関数は、keyを指定して、cookieを取得します
+ * @param {string} key cookieの名前
+ * @returns {string} 取得されたcookie
+ */
+function load(key) {
+	const value = document.cookie.match(`(^|;) ?${key}=([^;]*)(;|$)`);
+	return value ? value[2] : null;
+}
