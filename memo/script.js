b29f1511fd3508678c5e3891634ef3c9caafe1a5
とうもろほし <tomorohoshi0902@gmail.com>
2026-08-14 23:59:21 +0900

    バグを解消(3)

--- memo/script.js
+++ memo/script.js
@@ -169,7 +169,7 @@ document.addEventListener("DOMContentLoaded", () => { // ロード時にCookie�
 	if(document.cookie != "") {
 		const cookies = document.cookie.split("; ");
 		cookies.forEach(cookie => {
-			if(cookie.slice(0, 4) != "memo") continue;
+			if(cookie.slice(0, 4) != "memo") return;
 			const [cookieName, cookieValue] = cookie.split("=");
 			const isTitleCookie = Boolean(cookieName.split("Title")[1]); // キーに"Title"を含むか
 			let cookieMemoNum;
